c6807f197e575c8a9576d33287b154a341cc3ed6
Kayla Lamp <kayla.lamp@afrozaar.com>
2022-11-19 17:14:53 +0200

    Initial Laravel project setup

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => Str::ucfirst($this->faker->words(rand(1, 5), true)),
+        ];
+    }
+}
